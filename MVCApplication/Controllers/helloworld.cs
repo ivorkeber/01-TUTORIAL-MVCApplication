e2a3d76927e242f2750e88112ad6c665e695b199
Ivor Keber <ivkeber@vsite.hr>
2025-10-30 18:42:06 +0100

    pred 4

---
+++ MVCApplication/Controllers/helloworld.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MVCApplication.Controllers
+{
+    public class helloworld : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
